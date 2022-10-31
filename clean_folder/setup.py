2e1e8f355903b70b1f81af8b87cb0e42f934c29b
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2022-10-31 23:15:39 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='clean folder',
+    name='clean_folder',
     version='0.1',
-    packages=find_packages()
+    packages=find_packages(),
+    entry_points = {'console_scripts': ['clean-folder = clean_folder.clean:main']}
 )
+
