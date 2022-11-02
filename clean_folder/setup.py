ae218beebe61b2fa6bfd8bfbb591dcc9e3238f69
VadymBoyko <109297726+VadymBoyko@users.noreply.github.com>
2022-11-02 18:20:32 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,6 +4,6 @@ setup(
     name='clean_folder',
     version='0.1',
     packages=find_packages(),
-    entry_points = {'console_scripts': ['clean-folder = clean_folder.clean:main']}
+    entry_points = {'console_scripts': ['clean-folder = clean_folder.clean:start_proc_by_command_line']}
 )
 
